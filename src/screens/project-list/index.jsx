64824ea8bc79cf69c493fe63e7de66749ab40bf7
KendallBranchett <1325936293@qq.com>
2024-01-12 14:24:24 +0800

    feat: 实现并使用useMount, useDebounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,39 +1,44 @@
-import React from 'react';
+import React from "react";
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { useState, useEffect } from "react";
-import { cleanObject } from 'utils';
-import * as qs from "qs"
+import { cleanObject, useMount, useDebounce } from "utils";
+import * as qs from "qs";
 
-const apiUrl = process.env.REACT_APP_API_URL
+const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () => {
-    const [list, setList] = useState([])
-    const [users, setUsers] = useState([])
-    const [param, setParam] = useState({
-        name: '',
-        personId: ''
-    })
+  const [list, setList] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const debouncedParam = useDebounce(param, 1000);
 
-    useEffect(() => {
-      //当用户输入关键词或选择select框时,param变化，param变化时请求工程列表
-      fetch(`${apiUrl}/users`).then(async res => { 
-          if(res.ok) {
-            setUsers(await res.json())
-            console.log('users', users)
-          }
-      })
-    },[])
+  useMount(() => {
+    //当用户输入关键词或选择select框时,param变化，param变化时请求工程列表
+    fetch(`${apiUrl}/users`).then(async (res) => {
+      if (res.ok) {
+        setUsers(await res.json());
+        console.log("users", users);
+      }
+    });
+  });
 
-    useEffect(() => {
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async res => {
-            if(res.ok) {
-              setList(await res.json())
-            }
-        })
-      },[param])
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
+      async (res) => {
+        if (res.ok) {
+          setList(await res.json());
+        }
+      }
+    );
+  }, [debouncedParam]);
 
-    return <div>
-        <SearchPanel users={users} param={param} setParam={setParam}/>
-        <List users={users} list={list}/>
+  return (
+    <div>
+      <SearchPanel users={users} param={param} setParam={setParam} />
+      <List users={users} list={list} />
     </div>
-}
+  );
+};
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,44 @@
-export const isFalsy = (value) => value === 0 ? false : !value
+import { useEffect, useState } from "react";
+
+export const isFalsy = (value) => (value === 0 ? false : !value);
 
 //在函数中，改变传入的对象本事是不好的
 export const cleanObject = (object) => {
-    const result = {...object};
-    Object.keys(result).forEach(key => {
-        const value = result[key];
-        if (isFalsy(value)) {
-            delete result[key]
-        }
-    })
-    return result
-}
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    const value = result[key];
+    if (isFalsy(value)) {
+      delete result[key];
+    }
+  });
+  return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+// export const debounce = (func, delay) => {
+//   let timeout;
+//   return (...param) => {
+//     if (timeout) {
+//       clearTimeout(timeout);
+//     }
+//     timeout = setTimeout(function () {
+//       func(...param);
+//     }, delay);
+//   };
+// };
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    //每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    //每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debouncedValue;
+};
